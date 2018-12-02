fba466a461eef27c938c8cd3928d341cae2dc157
wj1203 <541569812@qq.com>
2018-12-02 13:17:08 +0800

    second

---
+++ app/src/main/java/com/example/yangyang/insect/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.yangyang.insect.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yangyang.insect.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/yangyang/insect/activity/SplashActivity.java
+++ app/src/main/java/com/example/yangyang/insect/activity/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.yangyang.insect.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -12,11 +14,21 @@ public class SplashActivity extends BaseActivity {
 
     ImageView iv_insect;
     TextView tv_chongqu;
+
+    private Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         initView();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        },4300);
     }
 
     private void initView() {
